afccc0b0d3fd54b03decb9f338693f1dd960cb68
Aleksundraaa <ladycaxap@gmail.com>
2025-04-11 11:38:04 +0500

    Выход из игры при нажатии на Quit

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
-using UnityEngine.UI; // Не забудьте добавить этот namespace для работы с UI
+using UnityEngine.UI; 
 using TMPro;
+using System.Collections; 
 
 public class MainMenuController : MonoBehaviour
 {
-    public Button[] menuButtons; // Массив кнопок
-    public TextMeshProUGUI[] menuTexts; // Массив текстов для отображения
+    public Button[] menuButtons; 
+    public TextMeshProUGUI[] menuTexts; 
     public AudioClip moveSound;
-    public AudioClip selectSound;
-    public GameObject optionsPanel; // Ссылка на панель настроек
+    public AudioClip selectSound; 
+    public GameObject optionsPanel; 
     private AudioSource audioSource;
     private int selectedIndex = 0;
 
@@ -20,7 +21,7 @@ public class MainMenuController : MonoBehaviour
 
     void Update()
     {
-        if (menuButtons.Length == 0) return; // Если массив пустой, ничего не делаем
+        if (menuButtons.Length == 0) return; 
 
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
@@ -35,7 +36,7 @@ public class MainMenuController : MonoBehaviour
             PlaySound(moveSound);
             UpdateMenu();
         }
-
+        
         if (Input.GetKeyDown(KeyCode.Return))
         {
             PlaySound(selectSound);
@@ -55,32 +56,13 @@ public class MainMenuController : MonoBehaviour
         {
             if (i == selectedIndex)
             {
-                // Добавляем символ '>' к выделенному пункту
                 menuTexts[i].text = ">" + menuTexts[i].text.TrimStart('>');
-                menuButtons[i].Select(); // Выбор кнопки для управления фокусом
+                menuButtons[i].Select(); 
             }
             else
             {
-                // Убираем символ '>' от невыделенных пунктов
                 menuTexts[i].text = menuTexts[i].text.TrimStart('>');
             }
-        }
-    }
-
-    void ShowOptionsPanel()
-    {
-        gameObject.SetActive(false); // Скрываем главное меню
-        optionsPanel.SetActive(true); // Активируем панель настроек
-        UpdateOptionsTextVisibility(true); // Показываем текст панели настроек
-    }
-
-    void UpdateOptionsTextVisibility(bool isVisible)
-    {
-        // Предполагаем, что у вас есть ссылки на тексты в optionsPanel
-        TextMeshProUGUI[] optionTexts = optionsPanel.GetComponentsInChildren<TextMeshProUGUI>();
-        foreach (var text in optionTexts)
-        {
-            text.gameObject.SetActive(isVisible); // Устанавливаем видимость текста
         }
     }
 
@@ -90,16 +72,26 @@ public class MainMenuController : MonoBehaviour
         {
             case 0:
                 Debug.Log("Start Game");
-                // Здесь можно добавить код для начала игры
                 break;
             case 1:
                 Debug.Log("Options");
-                ShowOptionsPanel(); // Переход на панель настроек
+                optionsPanel.SetActive(true); 
                 break;
             case 2:
                 Debug.Log("Quit Game");
-                Application.Quit();
+                PlaySound(selectSound); 
+                StartCoroutine(QuitGame()); 
                 break;
         }
+    }
+
+    IEnumerator QuitGame()
+    {
+        yield return new WaitForSeconds(selectSound.length/2);
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
