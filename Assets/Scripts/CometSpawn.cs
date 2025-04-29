cd6ca4ade530176a2f79baa422b18014e1ccdc18
Aleksundraaa <ladycaxap@gmail.com>
2025-04-29 11:52:11 +0500

    fixed bags

--- Assets/Scripts/CometSpawn.cs
+++ Assets/Scripts/CometSpawn.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CometSpawner : MonoBehaviour
@@ -7,11 +8,14 @@ public class CometSpawner : MonoBehaviour
     public Sprite cometSpriteLeft;
     public Sprite cometSpriteRight;
     public RectTransform planetRect;
-    public float spawnInterval = 2f; 
+    public float spawnInterval = 6f; 
     public float spawnRangeX = 8.0f;
     public float cometSpeed = 7.0f;
     private float screenHeight;
     private Coroutine spawnCoroutine;
+
+    private List<GameObject> activeCometsLeft = new List<GameObject>();
+    private List<GameObject> activeCometsRight = new List<GameObject>();
 
     private void Start()
     {
@@ -40,16 +44,33 @@ public class CometSpawner : MonoBehaviour
 
     private void SpawnComet()
     {
-        var spawnSide = Random.Range(0, 2);
-        var spawnX = spawnSide == 0 ? -spawnRangeX : spawnRangeX;
-        var spawnY = Camera.main.orthographicSize + 1; 
+        int spawnSide = Random.Range(0, 2);
+        Vector3 spawnPosition;
 
-        var spawnPosition = new Vector3(spawnX, spawnY, 0); 
+        if (spawnSide == 0) 
+        {
+            if (activeCometsLeft.Count < 2) 
+            {
+                spawnPosition = new Vector3(-spawnRangeX, Random.Range(screenHeight / 3, screenHeight / 2), 0); 
+                SpawnAndTrackComet(spawnPosition, spawnSide);
+            }
+        }
+        else 
+        {
+            if (activeCometsRight.Count < 2) 
+            {
+                spawnPosition = new Vector3(spawnRangeX, Random.Range(screenHeight / 2, screenHeight * 2 / 3), 0); 
+                SpawnAndTrackComet(spawnPosition, spawnSide);
+            }
+        }
+    }
 
+    private void SpawnAndTrackComet(Vector3 spawnPosition, int spawnSide)
+    {
         var comet = Instantiate(cometPrefab, spawnPosition, Quaternion.identity);
         var cometScript = comet.GetComponent<Comet>();
         cometScript.planetRect = planetRect;
-    
+
         var spriteRenderer = comet.GetComponent<SpriteRenderer>();
         if (spriteRenderer != null)
         {
@@ -61,12 +82,23 @@ public class CometSpawner : MonoBehaviour
             rigidBody = comet.AddComponent<Rigidbody2D>();
 
         rigidBody.gravityScale = 0;
-    
-        var diagonalSpeedX = spawnSide == 0 ? cometSpeed : -cometSpeed; 
-        var diagonalSpeedY = -cometSpeed; 
-        rigidBody.linearVelocity = new Vector2(diagonalSpeedX, diagonalSpeedY);
-    
-        Destroy(comet, (screenHeight + 10) / 
-                       Mathf.Sqrt(diagonalSpeedX * diagonalSpeedX + diagonalSpeedY * diagonalSpeedY));
+
+        var horizontalSpeed = spawnSide == 0 ? cometSpeed : -cometSpeed;
+        var verticalSpeed = -cometSpeed;
+
+        rigidBody.linearVelocity = new Vector2(horizontalSpeed, verticalSpeed);
+
+        if (spawnSide == 0)
+            activeCometsLeft.Add(comet);
+        else
+            activeCometsRight.Add(comet);
+
+        Destroy(comet, (screenHeight + 10) / Mathf.Sqrt(cometSpeed * cometSpeed));
+    }
+
+    private void Update()
+    {
+        activeCometsLeft.RemoveAll(comet => comet == null);
+        activeCometsRight.RemoveAll(comet => comet == null);
     }
 }
